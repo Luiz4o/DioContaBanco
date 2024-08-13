3a9b9b401efaf5b9890c7e28870000136ea580af
luizhenrique <henriqueluiz1103@gmail.com>
2024-08-13 19:48:08 -0300

    Projeto finalizado ContaBanco

--- src/Conta/ContaBanco.java
+++ src/Conta/ContaBanco.java
@@ -1,4 +1,40 @@
 package Conta;
 
 public class ContaBanco {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public ContaBanco(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nome: ") + nomeCliente + "\n" +
+                String.format("Agencia: ") + agencia + "\n" +
+                String.format("Numero: ") + numero + "\n" +
+                String.format("saldo: ") + saldo
+                ;
+    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,20 +1,56 @@
 package Main;
-
+import Conta.ContaBanco;
 import java.util.Scanner;
 
 public class Main {
+
+    private static Scanner read= new Scanner(System.in);
+    private static ContaBanco conta;
+
     public static void main(String[] args) {
-        Scanner read= new Scanner(System.in);
+
         int opcao=0;
 
         do{
-        System.out.println("Bem vindo ao seu Banco! \nO que deseja realizar hoje?");
+            System.out.println("===================================");
+        System.out.println("  Bem vindo ao seu Banco! \n  O que deseja realizar hoje?");
+            System.out.println("""
+                     1 - Criar conta
+                     2 - Consultar conta
+                     3 - Sair
+                    """);
+        opcao= read.nextInt();
+        read.nextLine();
+        switch(opcao){
+            case 1: criarConta();
+                    break;
+            case 2: consultarConta();
+                    break;
+        }
 
 
 
+        }while(opcao!=3);
+    }
 
+    private static void consultarConta() {
+        System.out.println(conta);
 
-        }while(opcao!=0);
+    }
+
+    private static void criarConta() {
+        System.out.println("Informe o nome do usuário:" );
+        String nome= read.nextLine();
+        System.out.println("Informe a agencia do usuario:" );
+        String agencia= read.nextLine();
+        System.out.println("Informe o número da conta:" );
+        int numero= read.nextInt();
+        System.out.println("Informe o saldo da conta:");
+        double saldo= read.nextDouble();
+
+        conta = new ContaBanco(numero,agencia,nome,saldo);
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque \n", conta.getNomeCliente(),conta.getAgencia(),conta.getNumero(),conta.getSaldo());
     }
 
 
