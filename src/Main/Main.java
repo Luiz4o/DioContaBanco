77995118ffa9f22fe5b200ef3a0812e277071043
luizhenrique <henriqueluiz1103@gmail.com>
2024-08-13 14:26:44 -0300

    commit inicial

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read= new Scanner(System.in);
+        int opcao=0;
+
+        do{
+        System.out.println("Bem vindo ao seu Banco! \nO que deseja realizar hoje?");
+
+
+
+
+
+        }while(opcao!=0);
+    }
+
+
+}
